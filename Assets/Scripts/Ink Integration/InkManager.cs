3f2ebaa960b5f4cbe500c3f788dd0cb10d124e3f
finch <blkfinch@gmail.com>
2022-02-12 13:00:27 -0700

    added ink integration and language selection for google

---
+++ Assets/Scripts/Ink Integration/InkManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class InkManager : MonoBehaviour
+{
+    public TextAsset inkAsset;
+    public Story activeStory;
+    public string currentStoryText;
+    public List<Choice> currentStoryChoices;
+
+    //Delegates for state changes in script
+    public delegate void OnStoryTextChangeEvent(string newText);
+    public static OnStoryTextChangeEvent storyTextChangeEvent;
+
+    public delegate void OnStoryChoiceChangeEvent(List<Choice> choices);
+    public static OnStoryChoiceChangeEvent onChoiceAvailable;
+
+    public static InkManager active;
+
+    private void Awake()
+    {
+        if(active == null) { active = this;  }
+        else { Destroy(this.gameObject);  }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeStory = new Story(inkAsset.text);
+        currentStoryChoices = new List<Choice>();
+
+        //set up error handling
+        activeStory.onError += (msg, type) => {
+            if (type == Ink.ErrorType.Warning)
+                Debug.LogWarning(msg);
+            else
+                Debug.LogError(msg);
+        };
+
+        RunStory();
+    }
+
+    public void ChooseAtIndex(int index)
+    {
+        activeStory.ChooseChoiceIndex(index);
+        RunStory();
+    }
+
+    public void JumpToScene(string name)
+    {
+        activeStory.ChoosePathString(name);
+        RunStory();
+    }
+
+    private void RunStory()
+    {
+        while (activeStory.canContinue)
+        {
+            currentStoryText = activeStory.ContinueMaximally();
+            OnStoryTextChange(currentStoryText);
+        }
+
+        if (activeStory.currentChoices.Count > 0)
+        {
+            for (int i = 0; i < activeStory.currentChoices.Count; ++i)
+            {
+                Choice choice = activeStory.currentChoices[i];
+                currentStoryChoices.Add(choice);
+            }
+
+            StoryChoiceAvailable(this.currentStoryChoices);
+        }
+    }
+
+    private void OnStoryTextChange(string newText)
+    {
+        storyTextChangeEvent(newText);
+    }
+
+    private void StoryChoiceAvailable(List<Choice> choices)
+    {
+        onChoiceAvailable(choices);
+    }
+}
