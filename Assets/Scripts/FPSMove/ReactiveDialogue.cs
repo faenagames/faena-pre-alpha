c07453c4d81d9132fd5ea3016efd047f922230d5
Connorses <onronc@gmail.com>
2022-03-17 15:25:19 -0600

    Addressable Audio Test

--- Assets/Scripts/FPSMove/ReactiveDialogue.cs
+++ Assets/Scripts/FPSMove/ReactiveDialogue.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ReactiveDialogue : ReactiveTarget
 {
+    [SerializeField]
+    private AudioManager audioManager;
     public override void OnRayClick()
     {
         Debug.Log("Clicked Person");
         GameReference.self.StartDialogue();
+        audioManager.PlaySound("Assets/Audio/testvoice");
     }
 }
